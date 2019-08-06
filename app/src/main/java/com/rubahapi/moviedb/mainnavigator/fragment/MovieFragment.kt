326ee2c8ca7211d382789b6ac40ce434113922d2
junifarhidayat <junifar@gmail.com>
2019-08-07 00:16:33 +0700

    Add favorites 80% done error di menunya

--- app/src/main/java/com/rubahapi/moviedb/mainnavigator/fragment/MovieFragment.kt
+++ app/src/main/java/com/rubahapi/moviedb/mainnavigator/fragment/MovieFragment.kt
@@ -1,15 +1,45 @@
 package com.rubahapi.moviedb.mainnavigator.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.view.*
 import com.rubahapi.moviedb.R
+import com.rubahapi.moviedb.adapter.SectionsPagerAdapter
 
 class MovieFragment: Fragment(){
+
+    lateinit var viewPager: ViewPager
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-//        return super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.activity_main_movie_fragment, container, false)
+        val viewLayout = inflater.inflate(R.layout.activity_main_movie_fragment, container, false)
+        val sectionsPagerAdapter = SectionsPagerAdapter(viewLayout.context, childFragmentManager)
+        viewPager = viewLayout.findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = viewLayout.findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+        val toolbar = viewLayout.findViewById<android.support.v7.widget.Toolbar>(R.id.main_toolbar)
+
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        toolbar.inflateMenu(R.menu.main_menu)
+        return viewLayout
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, menuInflater: MenuInflater?) {
+        menuInflater?.inflate(R.menu.main_menu, menu)
+        super.onCreateOptionsMenu(menu, menuInflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_change_settings){
+            val intent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
