0b760d4b60cbe2e362f1d4e57cf9c12899449ad2
junifarhidayat <junifar@gmail.com>
2019-08-26 20:55:58 +0700

    Update Widget

--- app/src/main/java/com/rubahapi/moviedb/main/fragment/movie/MoviePresenterView.kt
+++ app/src/main/java/com/rubahapi/moviedb/main/fragment/movie/MoviePresenterView.kt
@@ -3,6 +3,4 @@ package com.rubahapi.moviedb.main.fragment.movie
 import android.support.v4.app.Fragment
 
 interface MoviePresenterView<T: Fragment> {
-    fun onAttach(view: T)
-    fun onDetach()
 }
