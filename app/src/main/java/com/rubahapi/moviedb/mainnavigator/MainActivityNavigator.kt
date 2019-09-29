6847a961d89c3fe1417ed9e018e33e50eed6bbd0
junifar <junifar@gmail.com>
2019-09-30 00:56:54 +0700

    Sendnotifier kurang schedulenya

--- app/src/main/java/com/rubahapi/moviedb/mainnavigator/MainActivityNavigator.kt
+++ app/src/main/java/com/rubahapi/moviedb/mainnavigator/MainActivityNavigator.kt
@@ -1,11 +1,15 @@
 package com.rubahapi.moviedb.mainnavigator
 
-import android.app.SearchManager
+import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.provider.Settings
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.SearchView
 import android.view.Menu
@@ -13,23 +17,97 @@ import android.view.MenuItem
 import com.rubahapi.moviedb.R
 import com.rubahapi.moviedb.mainnavigator.fragment.MovieFragment
 import com.rubahapi.moviedb.mainnavigator.fragment.favorite.FavoriteFragment
+import com.rubahapi.moviedb.model.NotificationItem
+import java.util.*
+
 
 class MainActivityNavigator : AppCompatActivity(), SearchView.OnQueryTextListener{
 
     private lateinit var menuItem: MenuItem
     private val fm = supportFragmentManager
-//
-//    private fun newMovie(){
-//        val helper = DatabaseHelper(this, null, null, 1)
-//        val movie = Movie(1, "Title111", "overview", "path")
-//        helper.addMovie(movie)
-//    }
-//
-//    private fun newTVShow(){
-//        val helper = DatabaseHelper(this, null, null, 1)
-//        val tvShow = TvShow(1, "Title111", "overview", "path")
-//        helper.addTVShow(tvShow)
-//    }
+
+    companion object{
+        private const val NOTIF_REQUEST_CODE = 200
+        private const val CHANNEL_NAME = "Movie DB channel"
+        private const val GROUP_KEY_EMAILS = "group_key_emails"
+
+        private var idNotif = 0
+        private const val maxNotif = 2
+
+        private val stackNotif = ArrayList<NotificationItem>()
+    }
+
+    private fun sendNotif() {
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val largeIcon = BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px)
+        val intent = Intent(this, MainActivityNavigator::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            NOTIF_REQUEST_CODE,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val mBuilder: NotificationCompat.Builder
+
+        //Melakukan pengecekan jika idNotif lebih kecil dari Max Notif
+        val CHANNEL_ID = "channel_01"
+        if (idNotif < maxNotif) {
+            mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("New Email from " + stackNotif[idNotif].sender)
+                .setContentText(stackNotif[idNotif].message)
+                .setSmallIcon(R.drawable.ic_mail_white_48px)
+                .setLargeIcon(largeIcon)
+                .setGroup(GROUP_KEY_EMAILS)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+        } else {
+            val inboxStyle = NotificationCompat.InboxStyle()
+                .addLine("New Email from " + stackNotif[idNotif].sender)
+                .addLine("New Email from " + stackNotif[idNotif - 1].sender)
+                .setBigContentTitle("$idNotif new emails")
+                .setSummaryText("mail@dicoding")
+            mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("$idNotif new emails")
+                .setContentText("mail@dicoding.com")
+                .setSmallIcon(R.drawable.ic_mail_white_48px)
+                .setGroup(GROUP_KEY_EMAILS)
+                .setGroupSummary(true)
+                .setContentIntent(pendingIntent)
+                .setStyle(inboxStyle)
+                .setAutoCancel(true)
+        }
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel
+        Materi ini akan dibahas lebih lanjut di modul extended
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            /* Create or update. */
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            mBuilder.setChannelId(CHANNEL_ID)
+
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+
+        mNotificationManager.notify(idNotif, notification)
+    }
+
+    private val runnable = Runnable { sendNotifier() }
+
+    private fun sendNotifier(){
+        stackNotif.add(NotificationItem(idNotif, "Rubahapi", "Test"))
+        sendNotif()
+        idNotif++
+        Handler().postDelayed(runnable, 5000)
+    }
 
     override fun onQueryTextSubmit(search: String?): Boolean {
         return true
@@ -80,6 +158,8 @@ class MainActivityNavigator : AppCompatActivity(), SearchView.OnQueryTextListene
         if (savedInstanceState != null) {
             this.savedInstanceState = savedInstanceState
         }
+
+        sendNotifier()
 
         loadMovieFragment()
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
---
+++ app/src/main/java/com/rubahapi/moviedb/model/NotificationItem.kt
@@ -0,0 +1,7 @@
+package com.rubahapi.moviedb.model
+
+/**
+ * Created by dicoding on 1/21/2017.
+ */
+
+internal class NotificationItem(var id: Int, var sender: String, var message: String)
